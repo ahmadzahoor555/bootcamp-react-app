696bf27f4a34134f572812d5d65de8a390c393b9
Zahoor Ahmad <44691924+ahmadzahoor555@users.noreply.github.com>
2020-06-25 16:30:05 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello World! Welcom to Mardan
+    </div>
+  );
+}
+
+export default App;
